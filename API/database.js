9504d8672770502a7ca5d13bfe5ab092f8d87826
Justen Lane <justen.lane@gmail.com>
2023-04-05 15:25:11 -0400

    Promise.all create tables

--- API/database.js
+++ API/database.js
@@ -1,13 +1,18 @@
-const { Client } = require("pg");
+const { Pool } = require("pg");
 
-const client = new Client({
+const pool = new Pool({
   host: "localhost",
   user: "postgres",
   port: 5432,
   password: "rootUser",
   database: "qna",
 });
-create();
+
+createTables();
+async function createTables() {
+  await Promise.all[(createQuestions(), createAnswers(), createPhotos())];
+}
+
 //establish connection and create database
 
 const answerData = `COPY answers(id, question_id, body, date_written, answerer_name, answerer_email, reported, helpful)
@@ -25,12 +30,46 @@ FROM '/Users/Justen/Documents/Precourse/qnaAPI/answers_photos.csv'
 DELIMITER ','
 CSV HEADER;`;
 
-async function create() {
-  await client.connect();
+async function createQuestions() {
+  // create table for questions
+  await pool.query(`DROP TABLE IF EXISTS questions`);
+  await pool.query(
+      `CREATE TABLE questions (
+ id SERIAL PRIMARY KEY,
+ product_id VARCHAR(1000),
+ body VARCHAR(1000),
+ date_written VARCHAR(1000),
+ asker_name VARCHAR(50),
+ asker_email VARCHAR(50),
+ reported BOOLEAN,
+ helpful VARCHAR(1000)
+);`
+    )
+    .then((res) => console.log("created questions table"))
+    .then(() => pool.query(questionData))
+    .then((result) =>console.log("copied question data rowCount =", result.rowCount))
+    .catch((err) => console.log(err));
+}
+async function createPhotos() {
+  // create table for photos
+  await pool.query(`DROP TABLE IF EXISTS photos`);
+  await pool.query(
+      `CREATE TABLE photos (
+  id SERIAL PRIMARY KEY,
+  answer_id VARCHAR(1000),
+  url VARCHAR(1000)
+);`
+    )
+    .then((res) => console.log("created photos table"))
+    .then(() => pool.query(photoData)
+    .then((result) => console.log("copied photo data rowCount =", result.rowCount))
+    .catch((err) => console.log(err)))
+}
 
+async function createAnswers() {
   //create table for answers
-  await client.query(`DROP TABLE IF EXISTS answers`);
-  await client.query(
+  await pool.query(`DROP TABLE IF EXISTS answers`);
+  await pool.query(
     `CREATE TABLE answers (
   id SERIAL PRIMARY KEY,
   question_id VARCHAR(1000),
@@ -40,75 +79,10 @@ async function create() {
   answerer_email VARCHAR(50),
   reported BOOLEAN,
   helpful VARCHAR(1000)
-);`,
-    (err, res) => {
-      if (err) {
-        console.log("error in table create", err);
-      } else {
-        console.log("created answers table");
-      }
-    }
-  );
-  // create table for photos
-  await client.query(`DROP TABLE IF EXISTS photos`);
-  await client.query(
-    `CREATE TABLE photos (
-  id SERIAL PRIMARY KEY,
-  answer_id VARCHAR(1000),
-  url VARCHAR(1000)
-);`,
-    (err, res) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("created photos table");
-      }
-    }
-  );
-
-  // create table for questions
-  await client.query(`DROP TABLE IF EXISTS questions`);
-  await client.query(
-    `CREATE TABLE questions (
-  id SERIAL PRIMARY KEY,
-  product_id VARCHAR(1000),
-  body VARCHAR(1000),
-  date_written VARCHAR(1000),
-  asker_name VARCHAR(50),
-  asker_email VARCHAR(50),
-  reported BOOLEAN,
-  helpful VARCHAR(1000)
-);`,
-    (err, res) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("created questions table");
-      }
-    }
-  );
-
-  //Populate data in tables
-  await client
-    .query(photoData)
-    .then((result) => {
-      console.log("copied photo data rowCount =", result.rowCount);
-    })
-    .catch((err) => console.log(err));
-
-  await client
-    .query(questionData)
-    .then((result) => {
-      console.log("copied question data rowCount =", result.rowCount);
-    })
-    .catch((err) => console.log(err));
-
-  await client
-    .query(answerData)
-    .then((result) => {
-      console.log("copied answer data rowCount =", result.rowCount);
-    })
-    .catch((err) => console.log(err));
-
-    client.end()
+);`
+  )
+    .then((res) => console.log('created answers table'))
+    .then(() => pool.query(answerData)
+    .then((result) =>console.log("copied answer data rowCount =", result.rowCount))
+    .catch((err) => console.log(err)))
 }
